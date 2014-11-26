55021b0f48eb36f53afb3fb464a27f07abf3b4d5
themefuse <hello@themefuse.com>
2014-11-26 15:11:22 +0200

    first commit

---
+++ manifest.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined( 'FW' ) ) {
+	die( 'Forbidden' );
+}
+
+$manifest = array();
+
+$manifest['name']        = __( 'Analytics', 'fw' );
+$manifest['description'] = __( 'Enables the possibility to add the Google Analytics tracking code that will let you get all the analytics about visitors, page views and more.', 'fw' );
+$manifest['version'] = '1.0.0';
+$manifest['display'] = true;
+$manifest['standalone'] = true;
