84ed9aa4a40f65d4fac26bd95225cb24e1c0fd32
Andrei Glingeanu <andrei.glingeanu@gmail.com>
2016-11-10 19:16:40 +0200

    Update manifest.php

--- manifest.php
+++ manifest.php
@@ -11,3 +11,8 @@ $manifest['display'] = true;
 $manifest['standalone'] = true;
 
 $manifest['github_update'] = 'ThemeFuse/Unyson-Analytics-Extension';
+
+$manifest['github_repo'] = 'https://github.com/ThemeFuse/Unyson-Analytics-Extension';
+$manifest['uri'] = 'http://manual.unyson.io/en/latest/extension/introduction.html#content';
+$manifest['author'] = 'ThemeFuse';
+$manifest['author_uri'] = 'http://themefuse.com/';
